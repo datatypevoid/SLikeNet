7727251ac84ca6d024500207c5498c50b2de1026
David Newman <david.r.niciforovic@gmail.com>
2017-10-03 19:34:05 -0400

    chore: enable secure connections

--- Source/include/slikenet/NativeFeatureIncludesOverrides.h
+++ Source/include/slikenet/NativeFeatureIncludesOverrides.h
@@ -3,7 +3,7 @@
  *  All rights reserved.
  *
  *  This source code is licensed under the BSD-style license found in the
- *  RakNet License.txt file in the licenses directory of this source tree. An additional grant 
+ *  RakNet License.txt file in the licenses directory of this source tree. An additional grant
  *  of patent rights can be found in the RakNet Patents.txt file in the same directory.
  *
  */
@@ -14,6 +14,6 @@
 #ifndef __NATIVE_FEATURE_INCLDUES_OVERRIDES_H
 #define __NATIVE_FEATURE_INCLDUES_OVERRIDES_H
 
-//#define LIBCAT_SECURITY 1
+#define LIBCAT_SECURITY 1
 
 #endif
